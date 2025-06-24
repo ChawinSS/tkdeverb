5b83d290bf3cd5699f2d9f093a19bff975b7ca76
ChawinSS <137822600+ChawinSS@users.noreply.github.com>
2025-06-24 21:59:50 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,152 @@
+const quizDataFromFile = [
+  {
+    "question": "ben",
+    "correct": "ich",
+    "choices": [
+      "sowieso",
+      "Regisseur(in)",
+      "ich",
+      "selten"
+    ]
+  },
+  {
+    "question": "sen",
+    "correct": "du",
+    "choices": [
+      "Zug",
+      "du",
+      "ab",
+      "achtzig"
+    ]
+  },
+  {
+    "question": "lütfen",
+    "correct": "bitte (auffordernd)",
+    "choices": [
+      "früh",
+      "bitte (auffordernd)",
+      "Superlativpartikel",
+      "Regisseur(in)"
+    ]
+  },
+  {
+    "question": "öğretmen",
+    "correct": "Lehrer(in)",
+    "choices": [
+      "ich",
+      "fast",
+      "Lehrer(in)",
+      "bitte (auffordernd)"
+    ]
+  },
+  {
+    "question": "tren",
+    "correct": "Zug",
+    "choices": [
+      "bitte (auffordernd)",
+      "Zug",
+      "Regisseur(in)",
+      "ab"
+    ]
+  },
+  {
+    "question": "seksen",
+    "correct": "achtzig",
+    "choices": [
+      "achtzig",
+      "Regisseur(in)",
+      "fast",
+      "du"
+    ]
+  },
+  {
+    "question": "en",
+    "correct": "Superlativpartikel",
+    "choices": [
+      "selten",
+      "Superlativpartikel",
+      "manchmal",
+      "achtzig"
+    ]
+  },
+  {
+    "question": "hemen",
+    "correct": "gleich; sofort",
+    "choices": [
+      "ab",
+      "gleich; sofort",
+      "Zug",
+      "ich"
+    ]
+  },
+  {
+    "question": "zaten",
+    "correct": "sowieso",
+    "choices": [
+      "sowieso",
+      "du",
+      "ich",
+      "bitte (auffordernd)"
+    ]
+  },
+  {
+    "question": "-DEn itibaren",
+    "correct": "ab",
+    "choices": [
+      "manchmal",
+      "ab",
+      "Zug",
+      "Regisseur(in)"
+    ]
+  },
+  {
+    "question": "bazen",
+    "correct": "manchmal",
+    "choices": [
+      "ich",
+      "du",
+      "Regisseur(in)",
+      "manchmal"
+    ]
+  },
+  {
+    "question": "hemen hemen",
+    "correct": "fast",
+    "choices": [
+      "ab",
+      "ich",
+      "selten",
+      "fast"
+    ]
+  },
+  {
+    "question": "yönetmen",
+    "correct": "Regisseur(in)",
+    "choices": [
+      "Zug",
+      "fast",
+      "Regisseur(in)",
+      "achtzig"
+    ]
+  },
+  {
+    "question": "erken",
+    "correct": "früh",
+    "choices": [
+      "früh",
+      "Zug",
+      "ab",
+      "Superlativpartikel"
+    ]
+  },
+  {
+    "question": "nadiren",
+    "correct": "selten",
+    "choices": [
+      "manchmal",
+      "selten",
+      "ab",
+      "Superlativpartikel"
+    ]
+  }
+];
